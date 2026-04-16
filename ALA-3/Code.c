3ab1c5fb1e1fe61037baea2201b3f1867645acbc
nathanidhvanit-jpg <nathanidhvanit@gmail.com>
2026-04-16 11:50:37 +0530

    Add files via upload

---
+++ ALA-3/Code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n = 17, i, flag = 0;
+
+    for(i = 2; i <= n / 2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0 && n > 1)
+        printf("Prime\n");
+    else
+        printf("Not Prime\n");
+
+    return 0;
+}'
